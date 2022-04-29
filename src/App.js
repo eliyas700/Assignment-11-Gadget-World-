20964e7eebfbf911d1c525b6534eb96c461a5f32
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-29 12:19:22 +0600

    HomePage Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./Pages/Shared/Header/Header";
+import Home from "./Pages/HomePage/Home";
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/HomePage/Home.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Carousel } from "react-bootstrap";
+import "./Home.css";
+const Home = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <div>
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img
+            style={{ height: "88vh" }}
+            className="d-block w-100"
+            src="https://i.ibb.co/FqV1DGV/h5-s1-2048x.webp"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>iMac-2022</h3>
+            <p>Now available on Gadget World</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            style={{ height: "88vh" }}
+            className="d-block w-100"
+            src="https://i.ibb.co/PYZV3tT/h5-s2-2048x.webp"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Eco Bubble</h3>
+            <p>
+              Washes using 30% of the energy <br /> Bubbles do the rest
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            style={{ height: "88vh" }}
+            className="d-block w-100"
+            src="https://i.ibb.co/TY58rRG/h5-s3-2048x.webp"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Smart Watch Series-2</h3>
+            <p>Now Available on Gadget-World</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Home;
