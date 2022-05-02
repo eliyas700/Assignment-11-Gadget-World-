c2b4ad99df3033f2a97eb9c9567e5f374c568c37
Eliyas Hossain <eliyashossain700@gmail.com>
2022-05-02 11:29:22 +0600

    Google Sign In Implemented

--- src/Pages/Authentication/SocialLogin/Sociallogin.js
+++ src/Pages/Authentication/SocialLogin/Sociallogin.js
@@ -1,21 +1,37 @@
 import React from "react";
 import { FaFacebookSquare, FaGithub } from "react-icons/fa";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
 const Sociallogin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  if (user) {
+    navigate("/");
+    alert("Log in");
+  }
+  let errorMsg;
+  if (error) {
+    errorMsg = <p className="text-danger">{error?.message}</p>;
+  }
   return (
-    <div class="d-flex justify-content-center my-4">
-      <p class="social-label">Or login with</p>
-      <ul class="socials d-flex ">
-        <li>
-          <FaFacebookSquare color="#4867AA" size={30} />
-        </li>
-        <li>
-          <FcGoogle size={30} />
-        </li>
-        <li>
-          <FaGithub color="#1B1F23" size={30} />
-        </li>
-      </ul>
+    <div>
+      <p>{errorMsg}</p>
+      <div class="d-flex justify-content-center my-4">
+        <p class="social-label">Or login with</p>
+        <ul class="socials d-flex ">
+          <li>
+            <FaFacebookSquare color="#4867AA" size={30} />
+          </li>
+          <li onClick={() => signInWithGoogle()}>
+            <FcGoogle size={30} />
+          </li>
+          <li>
+            <FaGithub color="#1B1F23" size={30} />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
