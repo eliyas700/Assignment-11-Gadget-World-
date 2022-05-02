571b6ced610879bd3da54a4189b86c2f9aa122a8
Eliyas Hossain <eliyashossain700@gmail.com>
2022-05-02 15:04:01 +0600

    Create account By email and Password

--- src/Pages/Authentication/SignUp/Signup.js
+++ src/Pages/Authentication/SignUp/Signup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaUserAlt, FaLock } from "react-icons/fa";
 import { GrMail } from "react-icons/gr";
 import { FiLock } from "react-icons/fi";
@@ -6,7 +6,38 @@ import "./SignUp.css";
 import Sociallogin from "../SocialLogin/Sociallogin";
 import { Link } from "react-router-dom";
 import PageTitle from "../../Shared/PageTitle/PageTitle";
+import {
+  useCreateUserWithEmailAndPassword,
+  useUpdateProfile,
+} from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { async } from "@firebase/util";
 const Signup = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [updateProfile, updating, upadeProfileError] = useUpdateProfile(auth);
+  const [checked, setChecked] = useState(false);
+  if (user) {
+    alert("User Created");
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.pass.value;
+    const confirmPass = event.target.re_pass.value;
+    if (password.length < 6) {
+      return setErrorMsg("Password should Have At least 6 Characters");
+    }
+    if (password === confirmPass) {
+      await createUserWithEmailAndPassword(email, password);
+      await updateProfile({ displayName: name });
+    } else {
+      return setErrorMsg("Sorry! Password Didn't Match.");
+    }
+  };
   return (
     <section className="signup">
       <PageTitle title="Sign Up "></PageTitle>
@@ -14,7 +45,11 @@ const Signup = () => {
         <div className="signup-content row justify-content-between">
           <div className="m-0 col-12 col-lg-6">
             <h2 className=" py-3">Create Account</h2>
-            <form className="register-form w-75 mx-auto" id="register-form">
+            <form
+              onSubmit={handleSubmit}
+              className="register-form w-75 mx-auto"
+              id="register-form"
+            >
               <div className="form-group d-flex align-items-center">
                 <label className="me-2 " for="name">
                   <FaUserAlt />
@@ -23,6 +58,7 @@ const Signup = () => {
                   type="text"
                   name="name"
                   id="name"
+                  required
                   placeholder="Your Name"
                 />
               </div>
@@ -33,6 +69,7 @@ const Signup = () => {
                 <input
                   type="email"
                   name="email"
+                  required
                   id="email"
                   placeholder="Your Email"
                 />
@@ -44,6 +81,7 @@ const Signup = () => {
                 <input
                   type="password"
                   name="pass"
+                  required
                   id="pass"
                   placeholder="Password"
                 />
@@ -55,6 +93,7 @@ const Signup = () => {
                 <input
                   type="password"
                   name="re_pass"
+                  required
                   id="re_pass"
                   placeholder="Repeat your password"
                 />
@@ -64,9 +103,13 @@ const Signup = () => {
                   <input
                     className="form-check-input"
                     type="checkbox"
+                    onClick={() => setChecked(!checked)}
                     id="gridCheck"
                   />
-                  <label className="form-check-label" for="gridCheck">
+                  <label
+                    className={!checked ? "text-danger" : "text-success"}
+                    for="gridCheck"
+                  >
                     I accept the terms and Conditions of <b>Gadget-World</b>
                   </label>
                 </div>
@@ -76,11 +119,13 @@ const Signup = () => {
                   type="submit"
                   name="signup"
                   id="signup"
+                  disabled={!checked}
                   className="form-submit"
                   value="Register"
                 />
               </div>
             </form>
+            <p className="text-danger my-2">{errorMsg}</p>
             <Link to="/login" className="text-gadget text-start ">
               Already have an account?
             </Link>
